39399ec4adfb6fd1acd87cf09b0674c12484a2b4
gpj283476091 <gpk283476091@gmail.com>
2021-03-20 08:02:17 +0530

    karthik2

--- src/org/test/Alpha.java
+++ src/org/test/Alpha.java
@@ -5,8 +5,9 @@ private void write() {
 	System.out.println("write");
 
 }
-private void read() {
+private void read() throws InterruptedException {
 	System.out.println("read");
+	Thread.sleep(5000);
 
 }
 }
